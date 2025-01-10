9fcc53546d4c58c80c7fcc8deec75850d4cc2618
anhmuz <anhelina.muzychenko@gmail.com>
2025-01-10 18:53:24 +0200

    filter and sort data by any column using JavaScript

--- ContactManagerApplication/Models/Person.cs
+++ ContactManagerApplication/Models/Person.cs
@@ -6,10 +6,10 @@ namespace ContactManagerApplication.Models
     {
         [Ignore]
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = default!;
         public DateTime DateOfBirth { get; set; }
         public bool Married { get; set; }
-        public string Phone { get; set; }
+        public string Phone { get; set; } = default!;
         public decimal Salary { get; set; }
     }
 }
